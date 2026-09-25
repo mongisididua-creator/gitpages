198b7a7ac5c00f2d9a41f060f68ed1ee098d83a0
mongisididua-creator <mongisididua@gmail.com>
2026-09-25 18:13:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GITPAGE_CONFIG = {
   // Tempel URL Deployment Web App Google Apps Script yang berakhir dengan /exec
-  APPS_SCRIPT_URL: ''
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxY77Prt7jM0okQ25deNQb2PBIdi7Ja6SAFEPzVVP9xnyoDcjpKMRiLi9jkCtGFrsXp/exec'
 };
